49222d8a7a2a3000cb7d471b33243007bb8a27c7
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2024-10-05 20:05:52 +0700

    create index method for handling index url

--- app/Http/Controllers/PortalController.php
+++ app/Http/Controllers/PortalController.php
@@ -6,5 +6,14 @@ use Illuminate\Http\Request;
 
 class PortalController extends Controller
 {
-    //
+    public function index()
+    {
+        $data = [
+            'title' => 'Portal',
+            'content' => 'Welcome to the portal page!',
+        ];
+
+
+        return view('portal', $data);
+    }
 }

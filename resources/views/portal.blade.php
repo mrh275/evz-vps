6d4ba323c940c18d0ec67ee9e48bd7384499764f
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2024-10-05 20:06:33 +0700

    create portal page

---
+++ resources/views/portal.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.template')
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <h1 class="font-semibold text-2xl">Welcome to the Portal</h1>
+                <p>This is the portal page</p>
+            </div>
+        </div>
+    </div>
+@endsection

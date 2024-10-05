3fc97376237915514b07f792eebbe9b0e9deca2d
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2024-10-05 20:26:54 +0700

    added favicon and included navbar layout

--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -6,10 +6,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>{{ $title }}</title>
+    <link rel="shortcut icon" href="{{ url('storage/img/favicon.png') }}" type="image/png">
     @vite('resources/css/app.css')
 </head>
 
 <body>
+    @include('layouts.navbar')
     @yield('content')
 </body>
 

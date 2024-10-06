25217d5f40dd031e791adcd3ff802b31ba4899e7
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2024-10-06 17:36:33 +0700

    import bootstrap cdn

--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -6,13 +6,16 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>{{ $title }}</title>
-    <link rel="shortcut icon" href="{{ url('storage/img/favicon.png') }}" type="image/png">
-    @vite('resources/css/app.css')
+    <link rel="shortcut icon" href="{{ url('assets/img/favicon.png') }}" type="image/png">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <link rel="stylesheet" href="{{ url('assets/css/base.css') }}">
 </head>
 
 <body>
     @include('layouts.navbar')
     @yield('content')
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
 </body>
 
 </html>

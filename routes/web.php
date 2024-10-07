63652710f2955d7133a9d516013cc0878b08ddf0
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2024-10-07 18:25:02 +0700

    added new route for auth login

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\PortalController;
 use Illuminate\Support\Facades\Route;
 
@@ -15,3 +16,4 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', [PortalController::class, 'index'])->name('portal.index');
+Route::get('/login', [LoginController::class, 'index'])->name('auth.login');

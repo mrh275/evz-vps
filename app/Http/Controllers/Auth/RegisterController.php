9b9857b5daa52e41b36a71d6a4489363739f91a7
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2024-10-07 19:07:07 +0700

    create auth register controller

---
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class RegisterController extends Controller
+{
+    public function index()
+    {
+        $data = [
+            'title' => 'Register'
+        ];
+
+        return view("auth.register", $data);
+    }
+}

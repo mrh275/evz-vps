3a3dd6aaf5721d4041d93245b70e07f111623374
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2024-10-07 19:06:52 +0700

    modified some url

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -6,7 +6,7 @@
             <div class="col-lg-5 col-12">
                 <div id="auth-left">
                     <div class="auth-logo">
-                        <a href="index.html" class="align-items-center d-flex fw-bold">
+                        <a href="{{ url('') }}" class="align-items-center d-flex fw-bold">
                             <img src="{{ url('assets/img/EVZ-LOGO-AK-XS.png') }}" style="width: 42px; height: auto;" alt="Logo">
                             <span class="fs-3 ms-2 login-brand-text">Evariz Digital Solutions</span>
                         </a>
@@ -37,7 +37,7 @@
                         <button type="submit" class="mt-5 shadow-lg btn btn-primary btn-block btn-lg">Log in</button>
                     </form>
                     <div class="mt-5 text-lg text-center fs-4">
-                        <p class="text-gray-600">Don't have an account? <a href="auth-register.html" class="font-bold">Sign
+                        <p class="text-gray-600">Don't have an account? <a href="{{ url('register') }}" class="font-bold">Sign
                                 up</a>.</p>
                         <p><a class="font-bold" href="auth-forgot-password.html">Forgot password?</a>.</p>
                     </div>

6d0099b971e1b527e5a6d3fd5645b2c9d85b7378
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2024-10-07 19:07:17 +0700

    added route for auth register

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\RegisterController;
 use App\Http\Controllers\PortalController;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +18,4 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [PortalController::class, 'index'])->name('portal.index');
 Route::get('/login', [LoginController::class, 'index'])->name('auth.login');
+Route::get('/register', [RegisterController::class, 'index'])->name('auth.register');

cbf021bbaa7ef02814c7c2aa992d6616381f360e
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2024-10-08 21:59:44 +0700

    added icons assets and separate navbar element into component

--- resources/views/layouts/user-template.blade.php
+++ resources/views/layouts/user-template.blade.php
@@ -9,18 +9,21 @@
     <link rel="stylesheet" href="{{ url('assets/compiled/css/app.css') }}">
     <link rel="stylesheet" href="{{ url('assets/compiled/css/app-dark.css') }}">
     <link rel="stylesheet" href="{{ url('assets/compiled/css/iconly.css') }}">
+    <link rel="stylesheet" href="{{ url('assets/extensions/@fortawesome/fontawesome-free/css/all.min.css') }}">
 </head>
 
 <body>
     <script src="assets/static/js/initTheme.js"></script>
     <div class="app" id="app">
         @include('layouts.components.sidebar')
-        @yield('content')
+        <div id="main" class='layout-navbar navbar-fixed'>
+            @include('layouts.components.navbar')
+            @yield('content')
+        </div>
     </div>
     <script src="{{ url('assets/static/js/components/dark.js') }}"></script>
     <script src="{{ url('assets/extensions/perfect-scrollbar/perfect-scrollbar.min.js') }}"></script>
     <script src="{{ url('assets/compiled/js/app.js') }}"></script>
-    <script src="{{ url('assets/extensions/apexcharts/apexcharts.min.js') }}"></script>
     <script src="{{ url('assets/static/js/pages/dashboard.js') }}"></script>
 </body>
 

ba10913137b50b2285b44bef787dae5472727f48
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2024-10-09 22:45:38 +0700

    added route for each menu on user panel

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,9 @@ use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\User\InvoiceController;
 use App\Http\Controllers\Auth\RegisterController;
 use App\Http\Controllers\User\DashboardController;
+use App\Http\Controllers\User\DomainsController;
+use App\Http\Controllers\User\ServicesController;
+use App\Http\Controllers\User\TicketsController;
 
 /*
 |--------------------------------------------------------------------------
@@ -22,4 +25,8 @@ Route::get('/', [PortalController::class, 'index'])->name('portal.index');
 Route::get('/login', [LoginController::class, 'index'])->name('auth.login');
 Route::get('/register', [RegisterController::class, 'index'])->name('auth.register');
 Route::get('/user/dashboard', [DashboardController::class, 'index'])->name('user.dashboard');
+Route::get('user/services', [ServicesController::class, 'index'])->name('user.services');
+Route::get('user/domains', [DomainsController::class, 'index'])->name('user.domains');
 Route::get('user/invoices', [InvoiceController::class, 'index'])->name('user.invoices');
+Route::get('user/tickets', [TicketsController::class, 'index'])->name('user.tickets');
+Route::get('user/logout', [LoginController::class, 'logout'])->name('user.logout');

179e88473aeac5e45d01a9731406cb27f9d3d278
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2024-10-09 17:22:51 +0700

    added menu checking if invoice menu is active

--- resources/views/layouts/user-template.blade.php
+++ resources/views/layouts/user-template.blade.php
@@ -6,6 +6,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>{{ $title }}</title>
+
+    {{-- Section Invoices --}}
+    @if ($activeMenu == 'invoices')
+        <link rel="stylesheet" href="{{ url('assets/extensions/simple-datatables/style.css') }}">
+        <link rel="stylesheet" href="{{ url('assets/compiled/css/table-datatable.css') }}">
+    @endif
+
     <link rel="stylesheet" href="{{ url('assets/compiled/css/app.css') }}">
     <link rel="stylesheet" href="{{ url('assets/compiled/css/app-dark.css') }}">
     <link rel="stylesheet" href="{{ url('assets/compiled/css/iconly.css') }}">
@@ -25,6 +32,10 @@
     <script src="{{ url('assets/extensions/perfect-scrollbar/perfect-scrollbar.min.js') }}"></script>
     <script src="{{ url('assets/compiled/js/app.js') }}"></script>
     <script src="{{ url('assets/static/js/pages/dashboard.js') }}"></script>
+    @if ($activeMenu == 'invoices')
+        <script src="{{ url('assets/extensions/simple-datatables/umd/simple-datatables.js') }}"></script>
+        <script src="{{ url('assets/static/js/pages/simple-datatables.js') }}"></script>
+    @endif
 </body>
 
 </html>

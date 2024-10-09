e67fd5b4e79c37760134bda352225337b294dad5
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2024-10-09 17:24:16 +0700

    added route for invoice url

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,11 @@
 <?php
 
-use App\Http\Controllers\Auth\LoginController;
-use App\Http\Controllers\Auth\RegisterController;
-use App\Http\Controllers\PortalController;
-use App\Http\Controllers\User\DashboardController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PortalController;
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\User\InvoiceController;
+use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\User\DashboardController;
 
 /*
 |--------------------------------------------------------------------------
@@ -21,3 +22,4 @@ Route::get('/', [PortalController::class, 'index'])->name('portal.index');
 Route::get('/login', [LoginController::class, 'index'])->name('auth.login');
 Route::get('/register', [RegisterController::class, 'index'])->name('auth.register');
 Route::get('/user/dashboard', [DashboardController::class, 'index'])->name('user.dashboard');
+Route::get('user/invoices', [InvoiceController::class, 'index'])->name('user.invoices');

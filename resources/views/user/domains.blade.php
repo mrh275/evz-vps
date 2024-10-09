8ad45ba42aa1dae4f02c0ef96edbd41c1d88357e
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2024-10-09 22:45:04 +0700

    create domain page

---
+++ resources/views/user/domains.blade.php
@@ -0,0 +1,84 @@
+@extends('layouts.user-template')
+
+@section('content')
+    <div id="main-content">
+        <div class="page-heading">
+            <div class="page-title">
+                <div class="row">
+                    <div class="order-last col-12 col-md-6 order-md-1">
+                        <h3>Domains</h3>
+                        <p class="text-subtitle text-muted">Daftar seluruh alamat domain yang sdah disewa.</p>
+                    </div>
+                    <div class="order-first col-12 col-md-6 order-md-2">
+                        <nav aria-label="breadcrumb" class="breadcrumb-header float-start float-lg-end">
+                            <ol class="breadcrumb">
+                                <li class="breadcrumb-item"><a href="index.html">User</a></li>
+                                <li class="breadcrumb-item active" aria-current="page">Domains</li>
+                            </ol>
+                        </nav>
+                    </div>
+                </div>
+            </div>
+            <section class="section">
+                <div class="card">
+                    <div class="card-header">
+                        <h5 class="card-title">
+                            Daftar Domain
+                        </h5>
+                    </div>
+                    <div class="card-body">
+                        <table class="table table-striped" id="table1">
+                            <thead>
+                                <tr>
+                                    <th>Invoice</th>
+                                    <th>Items</th>
+                                    <th>Start Date</th>
+                                    <th>End Date</th>
+                                    <th>Status</th>
+                                    <th>Action</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>INV/EVZ/VPS-PMX/X/2024</td>
+                                    <td>sman1rawamerta.sch.id</td>
+                                    <td>09 Oktober 2024</td>
+                                    <td>08 Oktober 2025</td>
+                                    <td>
+                                        <span class="badge bg-success">Active</span>
+                                    </td>
+                                    <td>
+                                        <button class="btn btn-secondary icon">
+                                            <i class="fas fa-print"></i>
+                                        </button>
+                                        <button class="btn btn-info icon">
+                                            <i class="fas fa-undo"></i>
+                                        </button>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>INV/EVZ/VPS-PMX/X/2024</td>
+                                    <td>sman1rawamerta.sch.id</td>
+                                    <td>09 Oktober 2024</td>
+                                    <td>08 Oktober 2025</td>
+                                    <td>
+                                        <span class="badge bg-danger">Expired</span>
+                                    </td>
+                                    <td>
+                                        <button class="btn btn-secondary icon">
+                                            <i class="fas fa-print"></i>
+                                        </button>
+                                        <button class="btn btn-info icon">
+                                            <i class="fas fa-undo"></i>
+                                        </button>
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+
+            </section>
+        </div>
+    </div>
+@endsection

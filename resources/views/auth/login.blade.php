4920df3813e76527c63dfb660a32e27f7b30825f
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2024-10-13 15:59:55 +0700

    modified some class with conditional checking when error occured

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -13,28 +13,30 @@
                     </div>
                     <h1 class="auth-title">Log in.</h1>
                     <p class="mb-5 auth-subtitle">Silahkan login terlebih dahulu!</p>
-
-                    <form action="{{ url('auth/attempt') }}" method="POST">
+                    @if (session()->get('errorEmail'))
+                        {{ dd(session()->get('errorEmail')) }}
+                    @endif
+                    <form action="{{ route('auth.login.attempt') }}" method="POST">
                         @csrf
                         <div class="mb-4 form-group position-relative has-icon-left">
-                            <input type="email" class="form-control form-control-xl" placeholder="Email">
+                            <input type="email" name="email" class="form-control form-control-xl {{ session()->has('error') ? 'is-invalid' : '' }}" placeholder="Email">
                             <div class="form-control-icon">
-                                <i class="bi bi-person"></i>
+                                <i class="bi bi-envelope"></i>
                             </div>
                         </div>
                         <div class="mb-4 form-group position-relative has-icon-left">
-                            <input type="password" class="form-control form-control-xl" placeholder="Password">
+                            <input type="password" name="password" class="form-control form-control-xl {{ session()->has('error') ? 'is-invalid' : '' }}" placeholder="Password">
                             <div class="form-control-icon">
                                 <i class="bi bi-shield-lock"></i>
                             </div>
                         </div>
                         <div class="form-check form-check-lg d-flex align-items-end">
-                            <input class="form-check-input me-2" type="checkbox" value="" id="flexCheckDefault">
-                            <label class="text-gray-600 form-check-label" for="flexCheckDefault">
-                                Keep me logged in
+                            <input class="form-check-input me-2" type="checkbox" value="" id="show-password">
+                            <label class="text-gray-600 form-check-label" for="show-password">
+                                Show password
                             </label>
                         </div>
-                        <button type="submit" class="mt-5 shadow-lg btn btn-primary btn-block btn-lg">Log in</button>
+                        <button id="login-button" type="submit" class="mt-5 shadow-lg btn btn-primary btn-block btn-lg">Log in</button>
                     </form>
                     <div class="mt-5 text-lg text-center fs-4">
                         <p class="text-gray-600">Don't have an account? <a href="{{ url('register') }}" class="font-bold">Sign
@@ -52,3 +54,16 @@
 
     </div>
 @endsection
+
+@push('scripts')
+    <script>
+        if ({{ session()->has('error') }} === 1) {
+            Swal.fire({
+                title: 'Login failed!',
+                text: "{{ session()->get('message') }}",
+                icon: 'danger',
+                confirmButtonText: 'Close'
+            })
+        }
+    </script>
+@endpush

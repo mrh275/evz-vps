333ba9a1d905ec60e38b99bcdf266ef368c1b151
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2024-10-13 15:59:04 +0700

    create login method and modified logout method

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -2,22 +2,59 @@
 
 namespace App\Http\Controllers\Auth;
 
-use App\Http\Controllers\Controller;
+use Exception;
+use App\Models\User;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
+
     public function index()
     {
+        if (Auth::check()) {
+            return redirect()->route('user.dashboard');
+        }
+
         $data = [
             'title' => 'Login'
         ];
         return view('auth.login', $data);
     }
 
-    public function logout()
+    public function login(Request $request)
     {
-        auth()->logout();
+        $credentials = $request->validate([
+            'email' => 'required|email',
+            'password' => 'required'
+        ]);
+
+        try {
+            if (Auth::attempt($credentials)) {
+                $request->session()->regenerate();
+                $userResponse = User::where('email', $credentials['email'])->first();
+                $user = $userResponse->toArray();
+                $request->session()->put('user', $user);
+                return redirect()->route('user.dashboard');
+            } else {
+                return back()->with([
+                    'error' => true,
+                    'message' => 'Email atau password salah'
+                ]);
+            }
+        } catch (Exception $e) {
+            return back()->with([
+                'error' => true,
+                'message' => 'Email dan password tidak boleh kosong'
+            ]);
+        }
+    }
+
+    public function logout(Request $request)
+    {
+        $request->session()->forget('user');
+        $request->session()->flush();
         return redirect()->route('portal.index');
     }
 }

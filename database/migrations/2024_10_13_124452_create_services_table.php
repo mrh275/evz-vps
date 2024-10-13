27c035be748e3702d5b6b1783225bb17f3e7b012
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2024-10-13 21:30:38 +0700

    create model, migration, factory for manipulate db

--- database/migrations/2024_10_13_124452_create_services_table.php
+++ database/migrations/2024_10_13_124452_create_services_table.php
@@ -11,13 +11,13 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('services', function (Blueprint $table) {
             $table->uuid('id')->primary();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->foreignUuid('trx_id')->constrained();
+            $table->string('item_name');
+            $table->date('start_date');
+            $table->date('expire_date');
+            $table->enum('status', ['active', 'inactive', 'expired']);
             $table->timestamps();
         });
     }
@@ -27,6 +27,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('services');
     }
 };
--- database/migrations/2024_10_13_130410_create_tickets_table.php
+++ database/migrations/2024_10_13_130410_create_tickets_table.php
@@ -11,13 +11,13 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->uuid('id')->primary();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+        Schema::create('tickets', function (Blueprint $table) {
+            $table->id();
+            $table->foreignUuid('user_id')->constrained();
+            $table->string('title');
+            $table->text('description');
+            $table->enum('status', ['open', 'closed']);
+            $table->date('expire_date');
             $table->timestamps();
         });
     }
@@ -27,6 +27,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('tickets');
     }
 };

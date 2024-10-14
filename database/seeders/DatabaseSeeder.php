ee1fde66bdd6d20ba6959023db1a67669363a1fd
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2024-10-14 18:12:28 +0700

    create factory sedder for vps plans

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use App\Models\User;
+use App\Models\VpsPlans;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
 
@@ -16,10 +17,37 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Administrator',
-            'email' => 'admin@admin.com',
-            'password' => Hash::make('password'),
+        // User::factory()->create([
+        //     'name' => 'Administrator',
+        //     'email' => 'admin@admin.com',
+        //     'password' => Hash::make('password'),
+        // ]);
+
+        VpsPlans::factory()->create([
+            'name' => 'VPS Power Max 1',
+            'cpu' => '2 Core',
+            'ram' => '2 GB',
+            'storage' => '50 GB SSD',
+            'bandwidth' => '1 TB',
+            'price' => 175000,
+        ]);
+
+        VpsPlans::factory()->create([
+            'name' => 'VPS Power Max 2',
+            'cpu' => '4 Core',
+            'ram' => '4 GB',
+            'storage' => '80 GB SSD',
+            'bandwidth' => '1 TB',
+            'price' => 350000,
+        ]);
+
+        VpsPlans::factory()->create([
+            'name' => 'VPS Power Max 3',
+            'cpu' => '6 Core',
+            'ram' => '8 GB',
+            'storage' => '100 GB SSD',
+            'bandwidth' => '1 TB',
+            'price' => 525000,
         ]);
     }
 }

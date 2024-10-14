3318828e4fda18465e4f8f7a253fb1545b92ff68
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2024-10-14 18:10:53 +0700

    get all vps plan from db

--- app/Http/Controllers/PortalController.php
+++ app/Http/Controllers/PortalController.php
@@ -2,17 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\VpsPlans;
 use Illuminate\Http\Request;
 
 class PortalController extends Controller
 {
     public function index()
     {
+        $vpsPlan = VpsPlans::all();
         $data = [
             'title' => 'Portal',
             'content' => 'Welcome to the portal page!',
+            'vpsPlan' => $vpsPlan,
         ];
-
 
         return view('portal', $data);
     }

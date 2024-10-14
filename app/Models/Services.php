79aadc0e4f9dcda36c923690df9b0b2730dfa4b6
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2024-10-14 12:34:26 +0700

    Added helper for modified uuid and added guarded field

--- app/Models/Services.php
+++ app/Models/Services.php
@@ -2,10 +2,15 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Services extends Model
 {
-    use HasFactory;
+    use HasFactory, HasUuids;
+
+    protected $guarded = [
+        'id',
+    ];
 }

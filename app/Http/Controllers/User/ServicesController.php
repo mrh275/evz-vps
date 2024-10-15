3d0b74d9d3c1ae1c767f2e710870c96f8f410eda
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2024-10-15 11:07:44 +0700

    modified method to get data from database

--- app/Http/Controllers/User/ServicesController.php
+++ app/Http/Controllers/User/ServicesController.php
@@ -2,18 +2,22 @@
 
 namespace App\Http\Controllers\User;
 
-use App\Http\Controllers\Controller;
+use App\Models\User;
+use App\Models\Services;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class ServicesController extends Controller
 {
     public function index()
     {
-        $user = session('user');
+        $user = User::find(session()->get('user'));
+        $services = Services::where('user_id', $user[0]->id)->get();
         $data = [
             'title' => 'Services',
             'activeMenu' => 'services',
-            'user' => $user
+            'user' => $user[0],
+            'services' => $services
         ];
 
         return view('user.services', $data);

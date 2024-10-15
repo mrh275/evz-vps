133c430ce369b5dff21491091157b35f9b2715cb
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2024-10-15 17:42:57 +0700

    create and copy tripay callback method

---
+++ app/Http/Controllers/Tripay/TripayCallbackController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers\Tripay;
+
+use App\Models\Invoice;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Models\Transaction;
+use Illuminate\Support\Facades\Response;
+
+class TripayCallbackController extends Controller
+{
+    // Isi dengan private key anda
+    protected $privateKey = config('services.tripay.private_key_test');
+
+    public function handle(Request $request)
+    {
+        $callbackSignature = $request->server('HTTP_X_CALLBACK_SIGNATURE');
+        $json = $request->getContent();
+        $signature = hash_hmac('sha256', $json, $this->privateKey);
+
+        if ($signature !== (string) $callbackSignature) {
+            return Response::json([
+                'success' => false,
+                'message' => 'Invalid signature',
+            ]);
+        }
+
+        if ('payment_status' !== (string) $request->server('HTTP_X_CALLBACK_EVENT')) {
+            return Response::json([
+                'success' => false,
+                'message' => 'Unrecognized callback event, no action was taken',
+            ]);
+        }
+
+        $data = json_decode($json);
+
+        if (JSON_ERROR_NONE !== json_last_error()) {
+            return Response::json([
+                'success' => false,
+                'message' => 'Invalid data sent by tripay',
+            ]);
+        }
+
+        $merchant_ref = $data->merchant_ref;
+        $tripayReference = $data->reference;
+        $status = strtoupper((string) $data->status);
+
+        if ($data->is_closed_payment === 1) {
+            $invoice = Transaction::where('merchant_ref', $merchant_ref)
+                ->where('trx_id', $tripayReference)
+                ->where('status', '=', 'UNPAID')
+                ->first();
+
+            if (! $invoice) {
+                return Response::json([
+                    'success' => false,
+                    'message' => 'No invoice found or already paid: ' . $merchant_ref,
+                ]);
+            }
+
+            switch ($status) {
+                case 'PAID':
+                    $invoice->update(['status' => 'PAID']);
+                    break;
+
+                case 'EXPIRED':
+                    $invoice->update(['status' => 'EXPIRED']);
+                    break;
+
+                case 'FAILED':
+                    $invoice->update(['status' => 'UNPAID']);
+                    break;
+
+                default:
+                    return Response::json([
+                        'success' => false,
+                        'message' => 'Unrecognized payment status',
+                    ]);
+            }
+
+            return Response::json(['success' => true]);
+        }
+    }
+}

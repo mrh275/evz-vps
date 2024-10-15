67c67c3787e50d50ef97e40b10475e1c054a0ba0
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2024-10-15 11:21:26 +0700

    added query to fetch data from db

--- app/Http/Controllers/User/InvoiceController.php
+++ app/Http/Controllers/User/InvoiceController.php
@@ -2,18 +2,22 @@
 
 namespace App\Http\Controllers\User;
 
-use App\Http\Controllers\Controller;
+use App\Models\User;
+use App\Models\Transaction;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class InvoiceController extends Controller
 {
     public function index()
     {
-        $user = session('user');
+        $user = User::find(session()->get('user'));
+        $trx = Transaction::where('user_id', $user[0]->id)->with('vpsPlan')->get();
         $data = [
             'title' => 'Invoices',
             'activeMenu' => 'invoices',
-            'user' => $user
+            'user' => $user[0],
+            'trx' => $trx
         ];
 
         return view('user.invoices', $data);

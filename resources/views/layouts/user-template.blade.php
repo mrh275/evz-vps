11017d3412240cad239c8c8d9c189b6e4e7f7456
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2024-10-16 19:08:11 +0700

    added favicon

--- resources/views/layouts/user-template.blade.php
+++ resources/views/layouts/user-template.blade.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>{{ $title }}</title>
+    <link rel="shortcut icon" href="{{ url('assets/img/favicon.png') }}" type="image/png">
 
     {{-- Section Invoices --}}
     @if ($activeMenu == 'services' || $activeMenu == 'domains' || $activeMenu == 'invoices' || $activeMenu == 'tickets')
